d12d6e2a1fb8d4f5073c2637d8aed92b93a5dccb
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-12 02:19:28 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Music-Downloader
+Simple project to download music from Youtube.
+
+# Packages
+- [express](https://npmjs.com/package/express)
+- [ejs](https://npmjs.com/package/ejs)
+- [ytdl-core](https://npmjs.com/package/ytdl-core)
+- [youtube-sr](https://www.npmjs.com/package/youtube-sr)
+- [cors](https://www.npmjs.com/package/cors)
+
+# Created by Taps
+
+# Credits
+**❤ Thank's [Snowflake](https://github.com/Snowflake107)**
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const ytdl = require("ytdl-core");
+const app = express();
+const youtube = require('youtube-sr');
+const cors = require('cors');
+
+app.use(cors());
+
+async function downloadVideo(video, res) {
+        if (!video) return res.send('Fuck?');
+        youtube.search(video, { limit: 1 }).then(async video => {
+            if (!video[0]) return res.send('No video found!');
+
+            var stream = ytdl(video[0].id, { quality: 'highestaudio' });
+            stream.pipe(res);
+        }).catch(err => res.send('API Error!'));
+    
+}
+
+app.get("/", (req, res) => {
+    return res.send('mert cimke#4741');
+});
+
+app.get("/api/youtube/:title", async (req, res) => {
+    let video = decodeURIComponent(req.params.title);
+    if (!video) return res.send('Invalid Parameters!');
+    return await downloadVideo(video, res);
+});
+
+app.listen(process.env.PORT, () => console.log("API Running!"));
