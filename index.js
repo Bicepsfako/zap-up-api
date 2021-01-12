d50955b707771b003441fbbce5ca79309c641d4c
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-12 14:51:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ async function getVideoInfo(video, res) {
         await youtube.search(video, { limit: 1 }).then(async video => {
             if (!video[0]) return res.json({ error: 'No video found!' });
 
-            const videoData = await ytdl.getBasicInfo(video[0].url);
-            res.json({ videoData.videoDetails });
+            const videoData = await ytdl.getBasicInfo(video[0].url).videoDetails;
+            res.json({ videoData });
         }).catch(err => res.json({ error: 'API Error: ' + err }));
 };
 
