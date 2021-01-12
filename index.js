d8e14b760ffefde3c932c3631dad0ff1b34daecf
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-12 03:39:09 +0300

    Remove awaits

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.use(cors());
 
 async function downloadVideo(video, res) {
         if (!video) return res.send('Fuck?');
-        await youtube.search(video, { limit: 1 }).then(async video => {
+        youtube.search(video, { limit: 1 }).then(async video => {
             if (!video[0]) return res.send('No video found!');
 
-            var stream = await ytdl(video[0].id, { quality: 'highestaudio' });
-            await stream.pipe(res);
+            var stream = ytdl(video[0].id, { quality: 'highestaudio' });
+            stream.pipe(res);
         }).catch(err => res.send('API Error!'));
     
 }
