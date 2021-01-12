cc9a989c617b58fb2eec02d1818285af6b40a53b
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-12 16:38:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -54,8 +54,8 @@ app.get("/api/youtube/play/*", async (req, res) => {
     return await downloadVideo(video, res);
 });
 
-app.get("/api/youtube/search/:title", async (req, res) => {
-    let video = decodeURIComponent(req.params.title);
+app.get("/api/youtube/search/*", async (req, res) => {
+    let video = decodeURIComponent(req.params[0]);
     if (!video) return res.json({ error: 'Invalid Parameters!' });
     return await searchVideo(video, res);
 });
