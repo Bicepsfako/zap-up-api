71f02fa788ff221ed2c9786efd70bf852a51d060
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-13 21:29:49 +0300

    Update spotify.js

--- src/functions/spotify.js
+++ src/functions/spotify.js
@@ -6,10 +6,10 @@ const token = await Auth.get({
     client_secret: "e10cd4e285ae412e9e88a1b996977e41",
 });
 console.log(token);
-const spotify = new Spotify.Client(token);
 };
 
 Init();
+const spotify = new Spotify.Client(token);
 
 const searchVideo = async(video, res) => {
         if (!video) return res.json({ error: 'Wtf?' });
