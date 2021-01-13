de107167167826cb8b6c5deddd678cd7d3964569
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-13 21:35:46 +0300

    Update spotify.js

--- src/functions/spotify.js
+++ src/functions/spotify.js
@@ -1,18 +1,13 @@
 const Spotify = require("spotify-api.js");
 const Auth = new Spotify.Auth();
-const Init = async() => {
+
+const searchVideo = async(video, res) => {
+        if (!video) return res.json({ error: 'Wtf?' });
 const token = await Auth.get({
     client_id: "3d3e1615331a4076a367c9b947cf187d",
     client_secret: "e10cd4e285ae412e9e88a1b996977e41",
 });
-console.log(token);
-};
-
-Init();
 const spotify = new Spotify.Client(token);
-
-const searchVideo = async(video, res) => {
-        if (!video) return res.json({ error: 'Wtf?' });
 
         await spotify.tracks.search(video).then(async video => {
             if (!video[0]) return res.json({ error: 'No video found!' });
@@ -23,13 +18,24 @@ const searchVideo = async(video, res) => {
 
 const getVideoInfo = async(video, res) => {
         if (!video) return res.json({ error: 'Wtf?' });
-  
+const token = await Auth.get({
+    client_id: "3d3e1615331a4076a367c9b947cf187d",
+    client_secret: "e10cd4e285ae412e9e88a1b996977e41",
+});
+const spotify = new Spotify.Client(token);
+
         const info = await spotify.tracks.get(video);
         res.json(info);
 };
 
 const getPlaylist = async(playlist, res) => {
         if (!playlist) return res.json({ error: 'Wtf?' });
+const token = await Auth.get({
+    client_id: "3d3e1615331a4076a367c9b947cf187d",
+    client_secret: "e10cd4e285ae412e9e88a1b996977e41",
+});
+const spotify = new Spotify.Client(token);
+
         await spotify.playlists.get(playlist).then(async playlist => {
             if (!playlist) return res.json({ error: 'No playlist found!' });
 
