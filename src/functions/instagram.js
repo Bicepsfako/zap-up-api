b9e6f15c4a27472059ca3edeb9df36f027dec11b
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-17 21:45:21 +0300

    Update instagram.js

--- src/functions/instagram.js
+++ src/functions/instagram.js
@@ -2,9 +2,8 @@ const igm = require("instagram-fetcher");
 
 const getMedia = async(video, res) => {
         if (!video) return res.json({ error: 'Wtf?' });
-        const link = igm.download(video);
+        const link = igm.download(video).catch(err => res.json({ error: 'API Error: ' + err }));
         await link.pipe(res);
-        }).catch(err => res.json({ error: 'API Error: ' + err }));
 };
 
 
