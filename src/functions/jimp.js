ec7435a9c2967dd439542cf00d6bb53e4b25a0d5
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-23 15:17:05 +0300

    Update jimp.js

--- src/functions/jimp.js
+++ src/functions/jimp.js
@@ -7,7 +7,7 @@ const Render = async(video, res) => {
         if (!video) return res.json({ error: 'Wtf?' });
         let now = moment();
 		let minute = now.get('minute');
-		Jimp.read(`https://img.pngio.com/dark-forest-4k-ultra-hd-wallpapers-top-free-dark-forest-4k-ultra-dark-forest-hd-png-3840_2160.png`).then(function (delimg) {
+		Jimp.read(`https://wallpaperaccess.com/full/854063.jpg`).then(function (delimg) {
 			Jimp.loadFont(Jimp.FONT_SANS_64_WHITE).then(async function (font) {
 				await delimg.blur(50)
 				await delimg.resize(320, 320)
@@ -16,9 +16,9 @@ const Render = async(video, res) => {
 				// 15 sağa / // 80 yukarı
 				await delimg.print(font, 80, 20, moment().format('LT'), 80)
 				await delimg.print(font, 15, 80, moment().format('l'), 40)
-				await delimg.write(`./resimler/${minute}.png`);
-				await res.write(`./resimler/${minute}.png`);
-				fs.unlinkSync(`./resimler/${minute}.png`);
+				await delimg.write(`./resimler/${minute}.jpg`);
+				await res.write(`./resimler/${minute}.jpg`);
+				fs.unlinkSync(`./resimler/${minute}.jpg`);
 			});
 		});
 };
